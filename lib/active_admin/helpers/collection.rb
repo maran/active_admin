1ae88a145608623053dbb127ee4858c72ae001dc
Maran <maran.hidskes@gmail.com>
2015-01-18 11:12:17 +0100

    Work around nil problem

--- lib/active_admin/helpers/collection.rb
+++ lib/active_admin/helpers/collection.rb
@@ -4,20 +4,24 @@ module ActiveAdmin
       # 1. removes `select` and `order` to prevent invalid SQL
       # 2. correctly handles the Hash returned when `group by` is used
       def collection_size(c = collection)
-        if collection.first.class.included_modules.include?(Mongoid::Document)
-          if(not c.empty? and c.first.class.included_modules.include?(Mongoid::Document))
-            if c.first.class.embedded?
-               c.count
+        if collection.first.present?
+          if collection.first.class.included_modules.include?(Mongoid::Document)
+            if(not c.empty? and c.first.class.included_modules.include?(Mongoid::Document))
+              if c.first.class.embedded?
+                 c.count
+              else
+                c.count(true)
+              end
             else
-              c.count(true)
+              c.count
             end
           else
-            c.count
+            c = c.except :select, :order
+
+            c.group_values.present? ? c.count.count : c.count
           end
         else
-          c = c.except :select, :order
-
-          c.group_values.present? ? c.count.count : c.count
+          0
         end
       end
 
